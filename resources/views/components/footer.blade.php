78dffadae6452770d59293e6a6a6c818a49a6a2d
Wahyu Triono <whytrno@gmail.com>
2023-05-27 17:54:08 +0700

    Fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,29 +1,29 @@
-<div class="absolute bottom-0">
-    <div class="grid grid-cols-3 bg-blue-200">
-        <div>
+<div>
+    <div class="grid grid-cols-3 bg-blue-200 p-14 font-medium gap-24 ">
+        <div class="space-y-10">
             <h1 class="text-3xl font-bold">LOGO</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis et cumque sunt tempore. Vero,
                 cumque, exercitationem aperiam modi inventore deserunt ab adipisci iste consequuntur, impedit nihil.
                 Molestiae, ab tempora.</p>
         </div>
-        <div>
-            <a href="#">Home</a>
-            <a href="#">Diagnosa</a>
-            <a href="#">Hasil Diagnosa</a>
+        <div class="flex flex-col space-y-5">
+            <a href="#" class="w-min whitespace-nowrap">Home</a>
+            <a href="#" class="w-min whitespace-nowrap">Diagnosa</a>
+            <a href="#" class="w-min whitespace-nowrap">Hasil Diagnosa</a>
         </div>
-        <div>
-            <div>
+        <div class="space-y-5">
+            <div class="flex text-lg gap-5 items-center">
                 <img src="{{ url('icons/facebook.svg') }}" alt="">
                 <p>Raynaldi Zulfikar</p>
             </div>
-            <div>
+            <div class="flex text-lg gap-5 items-center">
                 <img src="{{ url('icons/linkedin.svg') }}" alt="">
                 <p>Raynaldi Zulfikar</p>
             </div>
         </div>
     </div>
 
-    <div class="bg-customRed">
+    <div class="bg-customRed p-5">
         <center>
             <div>
                 <p class="font-semibold text-lg text-white">© 2023 Raynaldi Zulfikar</p>
