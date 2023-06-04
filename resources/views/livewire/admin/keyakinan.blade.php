09840e2f0ca8c39fbd52187dff7f685f1048aa1a
Laurent Johanes <49395996+lxcoding666@users.noreply.github.com>
2023-06-05 05:06:18 +0700

    Update keyakinan.blade.php

--- resources/views/livewire/admin/keyakinan.blade.php
+++ resources/views/livewire/admin/keyakinan.blade.php
@@ -9,7 +9,7 @@
         <div class="px-5 w-full flex justify-between">
             <div class="flex gap-5 items-center">
                 <img src="{{ url('icons/admin/penyakit.svg') }}" alt="" class="h-10">
-                <h1 class="text-xl font-bold text-secondary2">Data penyakit & Solusi</h1>
+                <h1 class="text-xl font-bold text-secondary2">Data Keyakinan</h1>
             </div>
             <div class="text-xl flex items-center gap-5 bg-customGreen text-white px-4 rounded-md">
                 <img src="{{ url('icons/admin/plus.svg') }}" alt="">
@@ -21,19 +21,18 @@
             <thead>
                 <tr class="bg-blue-500 text-white font-bold text-xl">
                     <th class="px-5 py-4 w-20">No</th>
-                    <th class="px-5 py-4 w-44">Kode Penyakit</th>
-                    <th class="px-5 py-4">Nama Penyakit</th>
-                    <th class="px-5 py-4">Solusi</th>
+                    <th class="px-5 py-4 w-44">Keterangan</th>
+                    <th class="px-5 py-4">Nilai MB</th>
+                    <th class="px-5 py-4">Nilai MD</th>
                     <th class="px-5 py-4 w-40">Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 <tr class="text-secondary2 font-bold text-center border-b-2 border-secondary-2">
                     <td>1</td>
-                    <td>P1</td>
-                    <td>Busuk (Phytophtora infestans de Barry)</td>
-                    <td>Untuk mengatasi penyakit busuk pada tanaman tomat, langkah-langkah yang dapat dilakukan
-                        adalah dengan melakukan rotasi tanaman dan penyemprotan fungisida. </td>
+                    <td>Sangat Yakin</td>
+                    <td>0.2</td>
+                    <td>0.8</td>
                     <td class="flex justify-center gap-5">
                         <img src="{{ url('icons/admin/edit.svg') }}" alt="">
                         <img src="{{ url('icons/admin/delete.svg') }}" alt="">
