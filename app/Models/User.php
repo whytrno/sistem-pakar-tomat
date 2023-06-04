518270b1a197705cea7eca06f91e29caa8574c94
Laurent Johanes <49395996+lxcoding666@users.noreply.github.com>
2023-06-05 05:09:23 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
 }
