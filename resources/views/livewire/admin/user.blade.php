bb71e12417951939e9c96ff7fb6e6acbd74be2e3
Laurent Johanes <49395996+lxcoding666@users.noreply.github.com>
2023-06-05 05:07:37 +0700

    Update user.blade.php

--- resources/views/livewire/admin/user.blade.php
+++ resources/views/livewire/admin/user.blade.php
@@ -9,37 +9,99 @@
         <div class="px-5 w-full flex justify-between">
             <div class="flex gap-5 items-center">
                 <img src="{{ url('icons/admin/penyakit.svg') }}" alt="" class="h-10">
-                <h1 class="text-xl font-bold text-secondary2">Data penyakit & Solusi</h1>
+                <h1 class="text-xl font-bold text-secondary2">Data User</h1>
             </div>
             <div class="text-xl flex items-center gap-5 bg-customGreen text-white px-4 rounded-md">
                 <img src="{{ url('icons/admin/plus.svg') }}" alt="">
-                <h1>Tambah Data</h1>
+                <button id="tambahData">Tambah Data</button>
             </div>
         </div>
-
+        
         <table class="table-fixed w-full">
             <thead>
                 <tr class="bg-blue-500 text-white font-bold text-xl">
                     <th class="px-5 py-4 w-20">No</th>
-                    <th class="px-5 py-4 w-44">Kode Penyakit</th>
-                    <th class="px-5 py-4">Nama Penyakit</th>
-                    <th class="px-5 py-4">Solusi</th>
+                    <th class="px-5 py-4 w-44">Email</th>
                     <th class="px-5 py-4 w-40">Aksi</th>
                 </tr>
             </thead>
             <tbody>
+            @foreach($users as $s) 
                 <tr class="text-secondary2 font-bold text-center border-b-2 border-secondary-2">
-                    <td>1</td>
-                    <td>P1</td>
-                    <td>Busuk (Phytophtora infestans de Barry)</td>
-                    <td>Untuk mengatasi penyakit busuk pada tanaman tomat, langkah-langkah yang dapat dilakukan
-                        adalah dengan melakukan rotasi tanaman dan penyemprotan fungisida. </td>
+                <td>{{$s->id}}</td>
+                <td>{{$s->email}}</td>
                     <td class="flex justify-center gap-5">
                         <img src="{{ url('icons/admin/edit.svg') }}" alt="">
                         <img src="{{ url('icons/admin/delete.svg') }}" alt="">
                     </td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
+        <div class="modal" id="myModal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <div class="border-2 shadow-xl p-10 rounded-xl space-y-10" id="diagnosisResult">
+            <div class="flex justify-between">
+                <h1 class="text-2xl font-semibold text-primary">Tambah Data Penyakit</h1>
+            </div>
+
+            <div class="font-semibold text-xl border-2 py-4 border-x-0">
+            <style>
+    label {
+        font-weight: bold;
+        margin-top: 10px;
+    }
+
+    input[type="text"], input[type="password"] {
+        margin-top: 5px;
+        padding: 10px;
+        width: 100%;
+        border-radius: 10px;
+        border: 2px solid #ccc;
+    }
+
+    .form-button {
+        margin-top: 20px;
+    }
+</style>
+
+<form>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email">
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password">
+    <div class="form-button">
+        <button class="py-4 px-8 text-white rounded-md bg-customBlue"> Submit </button>
+        <button class="py-4 px-8 mr-4 text-white rounded-md bg-customRed"> Cancel </button>
+    </div>
+    </form>
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("tambahData");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
     </div>
 </div>
